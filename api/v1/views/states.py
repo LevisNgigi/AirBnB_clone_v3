2a6deb1e0241a5557e35599f5cb11516cb78201c
LevisNgigi <ngigilevis@gmail.com>
2023-04-28 10:06:07 +0000

    Corrections

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" 
-objects that handle all default RestFul API actions for States 
-"""
+""" objects that handle all default RestFul API actions for States """
 from models.state import State
 from models import storage
 from api.v1.views import app_views
@@ -24,7 +22,7 @@ def get_states():
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/state/get_id_state.yml', methods=['get'])
-def get_state_id(state_id):
+def get_state(state_id):
     """ Retrieves a specific State """
     state = storage.get(State, state_id)
     if not state:
@@ -36,7 +34,7 @@ def get_state_id(state_id):
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 @swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
-def delete_state_id(state_id):
+def delete_state(state_id):
     """
     Deletes a State Object
     """
@@ -72,7 +70,7 @@ def post_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 @swag_from('documentation/state/put_state.yml', methods=['PUT'])
-def put_state_id(state_id):
+def put_state(state_id):
     """
     Updates a State
     """
