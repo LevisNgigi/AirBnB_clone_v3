435570bda1ae5c1b50f1362f18102a0a08a5c545
LevisNgigi <ngigilevis@gmail.com>
2023-04-28 09:58:36 +0000

    New changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """ Flask Application """
-from os import environ
-from flask_cors import CORS
-from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import environ
 from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
-
-
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -22,6 +19,7 @@ def close_db(error):
     """ Close Storage """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """ 404 Error
@@ -31,6 +29,14 @@ def not_found(error):
         description: a resource was not found
     """
     return make_response(jsonify({'error': "Not found"}), 404)
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 
 if __name__ == "__main__":
